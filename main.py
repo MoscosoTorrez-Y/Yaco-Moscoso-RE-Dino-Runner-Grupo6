e9b75ab27998a60044d57fb870ef0cb43b302838
MoscosoTorrez-Y <yacopojasanmoscosotorrez@gmail.com>
2022-12-07 20:05:29 -0400

    Class 1: Dinosaur and Obstacle

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-
+from dino_runner.components.game import Game
 
 if __name__ == "__main__":
-     print("hello there...")
+     game = Game()
+     death_count = 0
+     while game.running:
+          if not game.playing:
+               game.show_menu()
