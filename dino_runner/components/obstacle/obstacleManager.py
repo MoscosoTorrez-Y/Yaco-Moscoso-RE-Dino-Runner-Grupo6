e8c6b203ab734950bf0e9de78125b6e5287130b7
MoscosoTorrez-Y <yacopojasanmoscosotorrez@gmail.com>
2022-12-09 17:58:20 -0400

    Classe 5

--- dino_runner/components/obstacle/obstacleManager.py
+++ dino_runner/components/obstacle/obstacleManager.py
@@ -1,17 +1,18 @@
 import pygame
-
 from dino_runner.components.obstacle.cactus import Cactus
-from dino_runner.utils.constants import SMALL_CACTUS
+from dino_runner.components.obstacle.terodactil import Terodactil
+from dino_runner.utils.constants import SMALL_CACTUS, BIRD
+import random
 
 class ObstacleManager:
-
     def __init__(self):
         self.obstacles = []
 
     def update(self, game):
-        if len(self.obstacles) == 0:
+        if len(self.obstacles) == 0:    
             self.obstacles.append(Cactus(SMALL_CACTUS))
-
+            self.obstacles.append(Terodactil(BIRD))
+                
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
@@ -32,10 +33,7 @@ class ObstacleManager:
                         pygame.time.delay(500)
                         game.playing = False 
                         game.death_count += 1
-                        break
-                else:
-                    self.obstacles.remove(obstacle)
-        
+                        break      
 
     def draw(self, screen):
         for obstacle in self.obstacles:
--- dino_runner/components/score_menu/text_utils.py
+++ dino_runner/components/score_menu/text_utils.py
@@ -8,7 +8,6 @@ black_color = (0, 0, 0)
 
 def get_score_element(points):
     font = pygame.font.Font(FONT_STYLE, 20)
-
     text = font.render('Points: '+ str(points), True, black_color)
     text_rect = text.get_rect()
     text_rect.center = (1000, 40)
